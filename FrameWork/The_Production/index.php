15a6c7ca839dec57738c3d704ca57abe2f12f9d8
Patrick ImProVision <2351449+PatrickImProVision@users.noreply.github.com>
2026-02-28 21:26:29 +0100

    Update: Files

--- FrameWork/The_Production/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Bootstrap the application from the public folder so users can visit
-// http://localhost/CodeIgniter/ without needing to include /public in the URL.
-
-require __DIR__ . '/public/index.php';
